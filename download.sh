e03396a443cc60e708623d1cfd7db8adf8d3cc38
yanagi <yanagisawa@enigmo.co.jp>
2016-12-11 17:08:31 +0900

    download.sh

--- download.sh
+++ download.sh
@@ -3,10 +3,10 @@
 
 # download data files and save them under /data
 cd data
-wget -x --load-cookies ./../cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/training.zip
-wget -x --load-cookies ./../cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/test.zip
-wget -x --load-cookies ./../cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/SampleSubmission.csv
-wget -x --load-cookies ./../cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/IdLookupTable.csv
+wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/training.zip
+wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/test.zip
+wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/SampleSubmission.csv
+wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/IdLookupTable.csv
 mv www.kaggle.com/c/facial-keypoints-detection/download/* .
 sudo apt-get install unzip
 unzip training.zip
